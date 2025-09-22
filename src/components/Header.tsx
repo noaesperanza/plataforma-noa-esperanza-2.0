ef9adc6bf710bf79fc877693fac03d0d1f97f547
noaesperanza <iaianoaesperanza@gmail.com>
2025-09-22 17:41:45 -0300

    Create Header.tsx
    Arquivo 14

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="bg-purple-600 text-white p-4 shadow-md">
+      <h1 className="text-xl font-semibold">Plataforma Nôa Esperanza</h1>
+    </header>
+  );
+};
