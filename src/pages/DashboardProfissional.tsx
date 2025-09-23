c2113bdd1c8e370eaf8b58fc4faba3b936d6a544
noaesperanza <iaianoaesperanza@gmail.com>
2025-09-23 07:33:20 -0300

    Create DashboardProfissional.tsx
    ARquivo 31

---
+++ src/pages/DashboardProfissional.tsx
@@ -0,0 +1,8 @@
+export default function DashboardProfissional() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Dashboard Profissional</h1>
+      <p className="mt-2 text-gray-600">Em construção...</p>
+    </div>
+  );
+}
