72caa39ffe2fe612667224edc771e64a4cd35f71
Ricardo Valença <iaianoaesperanza@gmail.com>
2025-09-24 13:18:10 -0300

    Ajusta rotas em routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 // src/routes.tsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NoaChatPage from "./pages/NoaChatPage";
 import DashboardPaciente from "./pages/DashboardPaciente";
@@ -13,30 +13,28 @@ import NotFound from "./pages/NotFound";
 
 export default function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        {/* Página inicial */}
-        <Route path="/" element={<Home />} />
+    <Routes>
+      {/* Página inicial */}
+      <Route path="/" element={<Home />} />
 
-        {/* Chat principal */}
-        <Route path="/chat" element={<NoaChatPage />} />
+      {/* Chat principal */}
+      <Route path="/chat" element={<NoaChatPage />} />
 
-        {/* Dashboards */}
-        <Route path="/paciente" element={<DashboardPaciente />} />
-        <Route path="/medico" element={<DashboardMedico />} />
-        <Route path="/profissional" element={<DashboardProfissional />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+      {/* Dashboards */}
+      <Route path="/paciente" element={<DashboardPaciente />} />
+      <Route path="/medico" element={<DashboardMedico />} />
+      <Route path="/profissional" element={<DashboardProfissional />} />
+      <Route path="/admin" element={<AdminDashboard />} />
 
-        {/* Relatório narrativo */}
-        <Route path="/relatorio" element={<RelatorioNarrativo />} />
+      {/* Relatório narrativo */}
+      <Route path="/relatorio" element={<RelatorioNarrativo />} />
 
-        {/* Configurações e Perfil */}
-        <Route path="/config" element={<Configuracoes />} />
-        <Route path="/perfil" element={<Perfil />} />
+      {/* Configurações e Perfil */}
+      <Route path="/config" element={<Configuracoes />} />
+      <Route path="/perfil" element={<Perfil />} />
 
-        {/* Página de erro 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+      {/* Página de erro 404 */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
